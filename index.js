3bd24b396c38a2340f61c0acf1a70b5be5a48ed8
Pascal Turbo 21 <seager.calaboca@gmail.com>
2025-02-21 10:01:53 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+'use strict';
+const request = require('request');
+const validUrl = require('valid-url');
+const parseTorrent = require('parse-torrent');
+const morgan = require('morgan');
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+	res.send('Try sending a url!');
+});
+
+app.get('/*', (req, res) => {
+	const url = req.params[0];
+	if (validUrl.isUri(url)) {
+		// URL was sent as param
+		getTorrent(url)
+			.then(data => {
+				res.header({
+					magnetURI: data.magnetURI
+				});
+				res.sendStatus(204);
+			})
+			.catch(err => {
+				console.log(err.stack);
+				const {name, message, statusCode} = err;
+				res.status(statusCode || 400)
+					.json({
+						error: {
+							name,
+							message
+						}
+					});
+				res.send(`${err.name}: ${err.message}`);
+			});
+	} else {
+		res.sendStatus(404);
+	}
+});
+
+function getTorrent(url) {
+	return new Promise((resolve, reject) => {
+		request({url, encoding: null, timeout: 1500}, (err, res, body) => {
+			const statusCode = res.statusCode;
+			const contentType = res.headers['content-type'];
+
+			if (statusCode !== 200) {
+				return reject(errorHandler('Request Failed', `Status Code: ${statusCode}`, statusCode));
+			} else if (!/^application\/x-bittorrent/.test(contentType)) {
+				return reject(errorHandler('Invalid content-type', `Expected application/x-bittorrent but received ${contentType}`));
+			} else if (err) {
+				return reject(errorHandler('Request Failed', `The following error was encountered: ${err}`));
+			}
+
+			const parsedData = parseTorrent(body);
+			const pick = (obj, ...props) => Object.assign({}, ...props.map(prop => ({[prop]: obj[prop]})));
+			const magnetData = pick(parsedData, 'name', 'infoHash', 'infoHashBuffer', 'announce');
+			const torrentData = pick(parsedData, 'name', 'created', 'comment', 'infoHash', 'announce', 'files');
+			const magnetURI = parseTorrent.toMagnetURI(magnetData);
+
+			return resolve({magnetURI, torrentData});
+		});
+	});
+}
+
+function errorHandler(name, message, statusCode = null) {
+	const stack = new Error(`${name} - ${message}`);
+	return {
+		name,
+		message,
+		stack,
+		statusCode
+	};
+}
+
+app.listen(port, () => {
+	console.log(`App listening on port ${port}.`);
+});
---
+++ readme.md
@@ -0,0 +1,41 @@
+<h1 align="center">
+	<br>
+	<img width="500" src="https://cdn.rawgit.com/matchai/magnetizer-api/3156da235baf08af7edbbd67fe06f3ed5d9c0b1a/media/logo-text.svg" alt="Magnetizer">
+	<br>
+	<br>
+</h1>
+
+> API - Converts direct links to torrents into magnet links
+
+## Install
+
+```
+$ npm install
+$ npm start
+```
+
+*A local instance of magnetizer will be running at http://localhost:8080.*
+
+## API
+
+#### <code>GET</code> /:url
+
+Returns the magnet URI and data for a torrent
+
+##### Parameters
+
+- __url__ – The full URL (including protocol) to a torrent file.
+
+##### Return format
+- __magnetURI__ – The complete magnetURI of the torrent file.
+- __torrentData__ – The torrent data attainable from the torrent header.
+  - __name__ – Name as specified by the torrent creator.
+  - __created__ – The date the torrent was created in ISO 8601 format.
+  - __comment__ – Comment as specified by the torrent creator.
+  - __infoHash__ – The SHA1 hash sums of the torrent's meta files.
+  - __announce__ – An array of URLs of all tracker announces for the torrent.
+  - __files__ – An array containing all files and their paths in the torrent.
+
+## License
+
+MIT © [Matan Kushner](https://matchai.me)
